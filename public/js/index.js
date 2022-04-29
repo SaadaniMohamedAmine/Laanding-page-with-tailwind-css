4d2198c6c83151ef269018e94fa5a49c462bff2a
Saadani Mohamed Amine <mohamedaminesaadani79@gmail.com>
2022-04-29 07:48:46 +0200

    Landing page iwth tailwind

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,22 @@
 //javascript for the page 
+console.log('Landing page with tailwind css') ;
+
+const menuBtn=document.getElementById("menu-button") ;
+const mobileMenu=document.getElementById("mobile-menu") ;
+
+
+
+//responsive navbar 
+menuBtn.addEventListener('click',()=>{
+    menuBtn.classList.toggle('open') ;
+    mobileMenu.classList.toggle('flex') ;
+    mobileMenu.classList.toggle('hidden')
+})
+
+
+
+AOS.init({
+    delay: 200,
+    duration: 1200,
+    once: false,
+  });
